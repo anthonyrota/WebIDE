bda238435d86fab9f3bcf70c475694841bccd4e9
PandaSandStudio <atornosiynoh@gmail.com>
2018-07-07 18:53:58 +1000

    delete unused

--- src/models/Scheduler/SchedulerTaskList/ISchedulerActions.ts
+++
@@ -1,6 +0,0 @@
-// import { ISchedulerTask } from 'src/models/Scheduler/IScheduler'
-
-// export interface ISchedulerActions {
-//   removeTask(task: ISchedulerTask<any>): void
-//   addTask(task: ISchedulerTask<any>): void
-// }
